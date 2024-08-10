6b29c2b001cc852db69208eb6f6678e96a4fd315
nguyenduytay <duytayx8@gmail.com>
2024-08-10 16:32:28 +0700

    day file code

---
+++ Bai_273/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_273
+{
+    //Convert a non-negative integer num to its English words representation.
+    internal class Program
+    {
+
+        public static string  NumberTowords(int num)
+        {
+            string number_read = "";
+            int dozens, unit_row;
+            if (num == 0)
+                return "Zero";
+            string[] below_20 = { "", "One", "Two", "Three", "Four","Five","Six","Seven","Eight","Nine","Ten",
+                                  "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen",
+                                   "Eighteen","Nineteen"};
+            string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+            string[] thousands = { "", "Thousand", "Million", "Billion" };
+            
+           
+                int a, b, c;
+                int index = 3;
+                 while(num > 0)
+                {
+                    a = num / (int)Math.Pow(10, 3*index);
+                    num = num % (int)Math.Pow(10, 3*index);
+                    b = a / 100;
+                    c = a % 100;
+                if (a>0)
+                    {
+                        if (b > 0)
+                        {
+                            number_read += below_20[b] + " " + "Hundred" + " ";
+                        }
+                        if(c>0)
+                        {
+                            if (c < 20)
+                            {
+                            number_read += below_20[c]+" ";
+                            }
+                            else if (c < 100)
+                            {
+
+                                dozens = c / 10;
+                                number_read += tens[dozens]+" ";
+                                unit_row = c % 10;
+                                if(unit_row>0)
+                                number_read += below_20[unit_row]+" ";
+                            }
+                        }
+                       number_read += thousands[index]+" ";
+                }
+                    index--;
+                }
+               number_read.Trim();
+                return number_read;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberTowords(1234567));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bai_2053/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_2053
+{
+    internal class Program
+    {
+        public static string kthDistinct(string[] arr, int k)
+        {
+            /*int index = 0;
+            bool kiemtra = false;
+            for (int i=0;i<arr.Length;i++)
+            {
+               for( int j=i+1;j<arr.Length;j++)
+                {
+                    if (arr[i].Equals(arr[j]))
+                    {
+                        arr[j] = "";
+                        kiemtra = true;
+                    }
+                    if(j==arr.Length-1)
+                    {
+                        if(kiemtra)
+                        {
+                            arr[i] = "";
+                            kiemtra = false;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!arr[i].Equals(""))
+                {
+                    index++;
+                    if (index == k)
+                    {
+                        return arr[i];
+                    }
+                }
+
+            }*/
+            int index = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                for(int j=0;j<arr.Length;j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                        break;
+                    if(j==arr.Length-1)
+                    {
+                        index++;
+                    }
+                }
+                if(index==k)
+                {
+                    return arr[i];
+                }
+            }
+                return "";
+        }
+        static void Main(string[] args)
+        {
+            string[] arr = { "a", "b", "a", "c" };
+            int k = 2;
+            Console.WriteLine(kthDistinct(arr,k));
+            Console.ReadLine();
+        }
+    }
+}
